3f6752fe42b1a111373c4b5c044ff9897d76f6cf
Nils Witt <nils@nils-witt.de>
2025-04-07 19:47:29 +0200

    Added a max Zoom for the map Added a Online BaseLayer

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,13 +7,21 @@ import {Config} from "./types/configType";
 const map = new LeafletMap("map");
 
 map.setView([50.722818, 7.14545], 13);
-
+map.setMaxZoom(22);
 
 const layersControl = new Control.Layers();
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 layersControl.addTo(map);
+
+
+const osm_online = new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    maxNativeZoom: 19,
+    maxZoom: 21,
+});
+layersControl.addBaseLayer(osm_online, 'OSM Online');
+osm_online.addTo(map);
 
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
